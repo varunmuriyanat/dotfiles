feb90c6ff3a507b6737e20cfeb2870f0f0ff0c81
Varun Muriyanat <varunmuriyanat@gmail.com>
2022-11-11 16:18:37 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,7 +13,6 @@ vim.opt.shiftwidth     = 4
 vim.opt.softtabstop    = 4
 vim.opt.splitright     = true
 vim.opt.tabstop        = 4
-vim.opt.virtualedit    = all
 vim.opt.wrap           = false
 
 vim.cmd [[
@@ -28,17 +27,28 @@ vim.cmd [[
     syntax enable
     set nocompatible
     set clipboard=unnamedplus
+    set virtualedit=all
 ]]
 
-vim.keymap.set('i', '<S-tab>',   '<C-d>')
-vim.keymap.set('i', '<tab>',     '<C-t>')
-vim.keymap.set('n', '*',         '*zz', {desc = 'Search and center screen'})
-vim.keymap.set('n', '<F2>',      '<cmd>Lexplore<cr>')
-vim.keymap.set('n', '<F5>',      ':buffers<CR>:buffer<Space>')
-vim.keymap.set('n', '<Leader>q', '<cmd>q<cr>')
-vim.keymap.set('n', '<Leader>w', ':write<CR>')
-vim.keymap.set('n', '<Space>',   ':')
-vim.keymap.set('n', '\\',        ':nohlsearch<CR>')
+vim.keymap.set('i', '<C-u>',       '<Esc>viwUea')
+vim.keymap.set('i', '<S-tab>',     '<C-d>')
+vim.keymap.set('i', '<tab>',       '<C-t>')
+vim.keymap.set('n', '*',           '*zz', {desc = 'Search and center screen'})
+vim.keymap.set('n', '<Backspace>', '<C-u>')
+vim.keymap.set('n', '<C-Down>',    'ddp')
+vim.keymap.set('n', '<C-Up>',      'ddkP')
+vim.keymap.set('n', '<Enter>',     '<C-d>')
+vim.keymap.set('n', '<F1>',        '<cmd>register<cr>')
+vim.keymap.set('n', '<F2>',        'q:')
+vim.keymap.set('n', '<F3>',        'q/')
+vim.keymap.set('n', '<F5>',        ':buffers<CR>:buffer<Space>')
+vim.keymap.set('n', '<Leader>f',   '<cmd>Lexplore<cr>')
+vim.keymap.set('n', '<Leader>q',   '<cmd>q<cr>')
+vim.keymap.set('n', '<Leader>w',   ':write<CR>')
+vim.keymap.set('n', '<Space>',     ':')
+vim.keymap.set('n', '\\',          ':nohlsearch<CR>')
+vim.keymap.set('v', '<C-Down>',    'xp`[V`]')
+vim.keymap.set('v', '<C-Up>',      'xkP`[V`]')
 
 local install_path    = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 local install_plugins = false
@@ -54,13 +64,10 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 require('packer').startup(function(use)
-    -- Package manager
-    use 'wbthomason/packer.nvim'
-
-    -- Theme inspired by Atom
-    use 'joshdick/onedark.vim'
-
+    use 'wbthomason/packer.nvim' -- Package manager
+    use 'joshdick/onedark.vim'   -- Theme inspired by Atom
     use 'nvim-lualine/lualine.nvim'
+    use 'neovim/nvim-lspconfig'  -- Configurations for Nvim LSP
 
     if install_plugins then
         require('packer').sync()
@@ -71,3 +78,4 @@ vim.opt.termguicolors = true
 vim.cmd('colorscheme onedark')
 
 require('lualine').setup()
+require('lspconfig').pyright.setup{}
